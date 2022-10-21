638312f858ca03c911b1cc4e8bb2e4af59c958ff
Masihdev <masihullahmassudi85@gmail.com>
2022-10-21 19:03:21 +0200

    can be now submitted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, PokemonComponent],
@@ -29,6 +30,7 @@ import { MatCardModule } from '@angular/material/card';
     MatFormFieldModule,
     MatToolbarModule,
     MatCardModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
